8a64252596b66226b240938e2c71f8f92cd9d520
Hobbestigrou <hobbestigrou@erakis.im>
2012-04-28 11:02:57 +0200

    [Scripts] Install: Install file in usr/local it's better.

--- install.sh
+++ install.sh
@@ -12,6 +12,8 @@ function check_user {
 
 function debian_install {
     local bin=$(which fortune)
+    local path_fortune='/usr/local/share/games/fortunes/'
+
     if [ ! $bin ]
     then
         echo 'Install fortune software'
@@ -19,11 +21,19 @@ function debian_install {
     fi
 
     strfile vimtips vimtips.dat
-    cp vimtips vimtips.dat /usr/share/games/fortunes/
+
+    if [ ! -d $path_fortune ]
+    then
+        mkdir -p $path_fortune
+    fi
+
+    cp vimtips vimtips.dat $path_fortune
 }
 
 function general_intall {
     local bin=$(which fortune)
+    locat path_fortune='/usr/local/share/fortunes/'
+
     if [ ! $bin ]
     then
         echo 'Need fortune software to work'
@@ -31,7 +41,13 @@ function general_intall {
     fi
 
     strfile vimtips vimtips.dat
-    cp vimtips vimtips.dat /usr/share/fortunes/
+
+    if [ ! -d $path_fortune ]
+    then
+        mkdir -p $path_fortune
+    fi
+
+    cp vimtips vimtips.dat $path_fortune
 }
 
 check_user
